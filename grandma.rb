93dc1c90eb899ab76ddae73af27441f4909b4ab3
Warda Ayaz <wayaz107@gmail.com>
2020-07-09 19:18:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,11 @@
 
 
 def speak_to_grandma (phrase)
-  if phrase == phrase.upcase && "I LOVE YOU GRANDMA!"
-       "NO, NOT SINCE 1938!"
+  if phrase == "I LOVE YOU GRANDMA!"
        "I LOVE YOU TOO PUMPKIN!"
  elsif phrase != phrase.upcase
       "HUH?! SPEAK UP, SONNY!"
+    else
+      "NO, NOT SINCE 1938!"
   end
 end
